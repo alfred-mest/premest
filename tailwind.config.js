3919d3560df48b3c06daacdce8d7d2ec49e957da
Alfred Delle <alfred.delle@yale.edu>
2020-10-09 18:09:28 +0000

    body color changed

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {},
+  theme: {
+    extend: {
+      colors: {
+        "body-color": '#949494'
+      }
+    },
+  },
+  purge: {
+    enabled: true,
+    content: ['./*.css', './*.html']
+  },
+  variants: {},
+  plugins: [],
+}
