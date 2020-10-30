bfa347fbfa44c40b70fd862f8adc0797a373afbc
Alfred Delle <alfred.delle@yale.edu>
2020-10-30 15:35:50 +0000

    New jobs

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 function getProfiles() {
   const menteeList = []
-  fetch('http://localhost:3000/cohort1')
+  fetch('https://intense-retreat-48313.herokuapp.com/cohort1')
     .then(res => res.json())
     .then(mentees => {
       menteeList.push(mentees)
       console.log(menteeList)
+    })
+    .catch(err => console.error(err));
+
+  const jobList = []
+  fetch('https://sheetdb.io/api/v1/j5h08ez2yjeqi')
+    .then(res => res.json())
+    .then(jobs => {
+      jobList.push(jobs)
+      console.log(jobList)
     })
     .catch(err => console.error(err));
 }
